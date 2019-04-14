5edbf69639cdc54540bf143eb1f02df12eaa40b8
jonahtash <jtash@umd.edu>
2019-04-14 07:57:32 -0400

    added twitter updating

--- app/app.py
+++ app/app.py
@@ -14,7 +14,7 @@ def create_app(config_object=settings):
     app.config.from_object(config_object)
 
 
-    gm = GoogleMaps(app, key="AIzaSyCt_HeBKWlD7QZg2jH4qSaqEMPYYflkxYU")
+    gm = GoogleMaps(app, key="REDACTED")
 
     register_extensions(app)
     register_blueprints(app)
